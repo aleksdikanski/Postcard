db504ad85255947a8b796447781182449b55324f
Aleksander Dikanski <aleksander.dikanski@qa82.de>
2015-07-07 21:21:04 +0200

    Debugging Connection Errors

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
